21ee4dc880ecc950cb7750837311ee0049e2a2f7
Clecio Jung <clecio.jung@gmail.com>
2021-11-07 15:59:00 -0300

    Add async library file

---
+++ async.js
@@ -0,0 +1,24 @@
+function async(genFn) {
+    const gen = genFn();
+    return function recursiveAsync(result) {
+        const { value, done } = gen.next(result);
+        if (done) return value;
+        try {
+            if (typeof value === "object" || typeof value === "function") {
+                const then = value.then;
+                if (then && typeof then === "function") {
+                    then.call(value,
+                        (result) => recursiveAsync(result),
+                        (reason) => gen.throw(reason)
+                    );
+                    return;
+                }
+            }
+            recursiveAsync(value);
+        } catch (error) {
+            gen.throw(error);
+        }
+    }
+}
+
+module.exports = async;
